9c454f5a20fa3ae78180f6a60b027e3043b2adff
BrianLM <brian.distefano@libertymutual.com>
2017-10-26 11:54:58 -0400

    Add functions

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,11 +1,21 @@
 'use strict'
+const handlebars = require('./templates/books-listing.handlebars')
 
 const getBook = function () {
-  // Add get book
+  apiCall.apiGet()
+    .then(showHandlebars)
+    .catch(bookErrors)
 }
 
-const showHandlebars = function () {
-  // add show response
+const showHandlebars = function (data) {
+  const showBookHtml = handlebars({books: data.books})
+  $('#info').empty()
+  $('#info').append(showBookHtml)
+}
+
+const bookErrors = function () {
+  $('#info').empty()
+  $('#info').append("<p>Dunno what'cha did, but ya did it wrong...</p>")
 }
 
 const addHandlers = function () {
