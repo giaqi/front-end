814a72b7d1a949c56e69fcfc46b4ec0bb848d378
BrianLM <brian.distefano@libertymutual.com>
2017-10-26 11:25:32 -0400

    Add basic functions

---
+++ assets/scripts/events.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const getBook = function () {
+  // Add get book
+}
+
+const showHandlebars = function () {
+  // add show response
+}
+
+const addHandlers = function () {
+  $('#best-button').on('click', getBook)
+}
+
+module.exports = {
+  addHandlers
+}
